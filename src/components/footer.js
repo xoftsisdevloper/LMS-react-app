cf1a847b327ffa8a057a1c9164c5ce5ae941bcf7
Abutalib <talibbca960@gmail.com>
2024-09-23 02:39:52 +0530

    first commit

---
+++ src/components/footer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div class="footer-bottom">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-5">
+                            <p class="copyright text-uppercase">Copyright © 2018
+                            </p>
+                        </div>
+                        <div class="col-sm-7">
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">About Us</a></li>
+                                <li><a href="#">Our services</a></li>
+                                <li><a href="#">Contact Us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+    );
+};
+
+export default Footer;
---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+// src/components/Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to the Home Page!</h2>
+      <p>This is a simple template without a backend.</p>
+    </div>
+  );
+};
+
+export default Home;
